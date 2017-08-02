44eac19c36b2d0df2433b8431d879c517310e03c
Jesús González <gopixc@gmail.com>
2017-08-01 19:26:13 -0500

    commit  db bakcup

--- sql/rgdiaz_04Jul17.sql
+++ sql/rgdiaz_04Jul17.sql
@@ -4,6 +4,32 @@
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.1.21-MariaDB
 
+
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.18
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 03, 2017 at 01:11 PM
+-- Server version: 5.6.35-cll-lve
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rdiaz`
+--
+CREATE DATABASE IF NOT EXISTS `rgdiaz` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `rgdiaz`;
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
