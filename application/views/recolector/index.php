aeca36bef638f532d121d3b7f53f06b42b960b23
Jesús González <gopixc@gmail.com>
2018-04-05 00:57:16 -0500

    Recolector index

--- application/views/recolector/index.php
+++ application/views/recolector/index.php
@@ -1,1 +1,11 @@
-index recolector
+<div class="span9"> 
+<center><legend>Manifiestos</legend></center> 
+ 
+<div class="well"> 
+
+  <form method='post' action="<?php echo site_url('recolector/index');?>"> 
+     
+  </form> 
+</div> 
+   
+</div>
