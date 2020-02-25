c01e56bd44889838edfa911a1b6988bce51489d6
Chuy <gopixc@gmail.com>
2020-02-25 00:20:49 -0600

    new db changes alta destino

--- application/views/administrador/recolector/alta.php
+++ application/views/administrador/recolector/alta.php
@@ -17,14 +17,14 @@
 								<div class="form-group row">
 									<label class="col-lg-3 col-form-label form-control-label" for="nombre_recolector">Nombre</label>
 									<div class="col-lg-9">
-										<input class="form-control" type="text" class="txt " style="width:85%; text-align: center;" name="nombre_recolector" id="nombre_recolector"  oninvalid="this.setCustomValidity('Ingresa nombre del recolector')" oninput="setCustomValidity('')"  required>
+										<input class="form-control" type="text" class="txt " style="width:81%; text-align: center;" name="nombre_recolector" id="nombre_recolector"  oninvalid="this.setCustomValidity('Ingresa nombre del recolector')" oninput="setCustomValidity('')"  required>
 									</div>
 								</div>
 
 								<div class="form-group row">
 									<label class="col-lg-3 col-form-label form-control-label" for="correo">Correo Electronico</label>
 									<div class="col-lg-9">
-										<input class="form-control" type="email" class="txt" style="width:85%; text-align: center;" name="correo" id="correo"  oninvalid="this.setCustomValidity('Ingresa correo')" oninput="setCustomValidity('')" required>
+										<input class="form-control" type="email" class="txt" style="width:81%; text-align: center;" name="correo" id="correo"  oninvalid="this.setCustomValidity('Ingresa correo')" oninput="setCustomValidity('')" required>
 									</div>
 								</div>
 
@@ -68,28 +68,28 @@
 								<div class="form-group row">
 									<label class="col-lg-3 col-form-label form-control-label" for="modelo">Modelo</label>
 									<div class="col-lg-9">
-										<input class="form-control" type="text" class="txt " style="width:85%; text-align: center;" name="modelo" id="modelo"  oninvalid="this.setCustomValidity('Ingresa moodelo del vehiculo')" oninput="setCustomValidity('')"  required>
+										<input class="form-control" type="text" class="txt " style="width:81%; text-align: center;" name="modelo" id="modelo"  oninvalid="this.setCustomValidity('Ingresa moodelo del vehiculo')" oninput="setCustomValidity('')"  required>
 									</div>
 								</div>
 
 								<div class="form-group row">
 									<label class="col-lg-3 col-form-label form-control-label" for="marca">Marca</label>
 									<div class="col-lg-9">
-										<input class="form-control" type="text" class="txt" style="width:85%; text-align: center;" name="marca" id="marca"  oninvalid="this.setCustomValidity('Ingresa marca')" oninput="setCustomValidity('')" required>
+										<input class="form-control" type="text" class="txt" style="width:81%; text-align: center;" name="marca" id="marca"  oninvalid="this.setCustomValidity('Ingresa marca')" oninput="setCustomValidity('')" required>
 									</div>
 								</div>
 
 								<div class="form-group row">
 									<label class="col-lg-3 col-form-label form-control-label" for="tipo">Tipo</label>
 									<div class="col-lg-9">
-										<input class="form-control" type="text" class="txt" style="width:85%; text-align: center;" name="tipo" id="tipo"  oninvalid="this.setCustomValidity('Ingresa correo')" oninput="setCustomValidity('')" required>
+										<input class="form-control" type="text" class="txt" style="width:81%; text-align: center;" name="tipo" id="tipo"  oninvalid="this.setCustomValidity('Ingresa correo')" oninput="setCustomValidity('')" required>
 									</div>
 								</div>
 
 								<div class="form-group row">
 									<label class="col-lg-3 col-form-label form-control-label" for="placa">No. Placa</label>
 									<div class="col-lg-9">
-										<input class="form-control" type="text" class="txt" style="width:85%; text-align: center;" name="placa" id="placa"  oninvalid="this.setCustomValidity('Ingresa correo')" oninput="setCustomValidity('')" required>
+										<input class="form-control" type="text" class="txt" style="width:81%; text-align: center;" name="placa" id="placa"  oninvalid="this.setCustomValidity('Ingresa correo')" oninput="setCustomValidity('')" required>
 									</div>
 								</div>
 
@@ -108,14 +108,51 @@
 				<div class="card">
 					<div class="card-header" id="headingTwo">
 						<h2 class="mb-0">
-							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
 								Alta Destino
 							</button>
 						</h2>
 					</div>
-					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionAltaRecolector">
+					<div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionAltaDestino">
 						<div class="card-body">
-							
+							<form class="form" role="form" autocomplete="off" method='post' action="<?php echo site_url('administrador/recolector_alta_vehiculo');?>">
+								<div class="form-group row">
+									<label class="col-lg-3 col-form-label form-control-label" for="nombre_destino">Nombre Empresa Destino</label>
+									<div class="col-lg-9">
+										<input class="form-control" type="text" class="txt " style="width:81%;" name="nombre_destino" id="nombre_destino"  oninvalid="this.setCustomValidity('Ingresa moodelo de la Empresa Destino')" oninput="setCustomValidity('')"  required>
+									</div>
+								</div>
+
+								<div class="form-group row">
+									<label class="col-lg-3 col-form-label form-control-label" for="numero_autorizacion">Numero de autorización</label>
+									<div class="col-lg-9">
+										<input class="form-control" type="text" class="txt" style="width:81%; text-align: center;" name="Número de Autorización" id="numero_autorizacion" name="numero_autorizacion" oninvalid="this.setCustomValidity('Ingresa No de autorización. ej: 06-09-ll-01-2011')" oninput="setCustomValidity('')" required>
+									</div>
+								</div>
+
+								<div class="form-group row">
+									<label class="col-lg-3 col-form-label form-control-label" for="calle">Calle</label>
+									<div class="col-lg-9">
+										<input class="form-control" type="text" class="txt" style="width:81%; text-align: center;" name="calle" id="calle" oninvalid="this.setCustomValidity('Ingresa calle')" oninput="setCustomValidity('')" required>
+									</div>
+								</div>
+
+								<div class="form-group row">
+									<label class="col-lg-3 col-form-label form-control-label" for="num_ext">Núm. Ext.</label>
+									<div class="col-lg-9">
+										<input class="form-control" type="text" class="txt" style="width:81%; text-align: center;" name="num_ext" id="num_ext" oninvalid="this.setCustomValidity('Ingresa el número exteriror')" oninput="setCustomValidity('')" required>
+									</div>
+								</div>
+
+								<br>
+
+								<div class="form-group row">
+									<label class="col-lg-3 col-form-label form-control-label"></label>
+									<div class="col-lg-9">
+										<input type="submit" class="btn btn-primary" value="Guardar">
+									</div>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
