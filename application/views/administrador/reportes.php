d9b8e3b1fe1d374d95fd955fcdc668dc363a28df
chuy <gopixc@gmail.com>
2022-06-03 09:02:03 -0500

    Moving to Lap

--- application/views/administrador/reportes.php
+++ application/views/administrador/reportes.php
@@ -5,46 +5,47 @@
 <div class="card card-white">
 	<div id="main-wrapper">
 		<div class="card-body">
-
-			<div class="col-md-6">
-				<div class="card-body">
-					<div class="card-heading clearfix">
-						<h4 class="card-title">Stacked Area Chart</h4>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card-body">
+						<div class="card-heading clearfix">
+							<h4 class="card-title">Stacked Area Chart</h4>
+						</div>
+						<canvas id="chart1">
+						</canvas>
 					</div>
-					<canvas id="chart1">
-					</canvas>
 				</div>
-			</div>
 
-			<div class="col-md-6">
-				<div class="card-body">
-					<div class="card-heading clearfix">
-						<h4 class="card-title">chart2</h4>
+				<div class="col-md-6">
+					<div class="card-body">
+						<div class="card-heading clearfix">
+							<h4 class="card-title">chart2</h4>
+						</div>
+						<canvas id="chart2">
+						</canvas>
 					</div>
-					<canvas id="chart2">
-					</canvas>
 				</div>
-			</div>
 
-			<div class="col-md-6">
-				<div class="card-body">
-					<div class="card-heading clearfix">
-						<h4 class="card-title">chart3</h4>
+				<div class="col-md-6">
+					<div class="card-body">
+						<div class="card-heading clearfix">
+							<h4 class="card-title">chart3</h4>
+						</div>
+						<canvas id="chart3">
+						</canvas>
 					</div>
-					<canvas id="chart3">
-					</canvas>
 				</div>
-			</div>
 
-			<div class="col-md-6">
-				<div class="card-body">
-					<div class="card-heading clearfix">
-						<h4 class="card-title">chart4</h4>
+				<div class="col-md-6">
+					<div class="card-body">
+						<div class="card-heading clearfix">
+							<h4 class="card-title">chart4</h4>
+						</div>
+						<canvas id="chart4">
+						</canvas>
 					</div>
-					<canvas id="chart4">
-					</canvas>
 				</div>
-			</div>
+			</div>	
 		</div>
 	</div>
 </div>
